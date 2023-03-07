5a6c6188e46c02168b5327017ce22ad3610f9664
47188inesmartins <A47188@alunos.isel.pt>
2023-03-07 21:57:37 +0000

    project

---
+++ src/test/kotlin/ps/schedule/project/ProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package ps.schedule.project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
