4cd40d921f24ac3aa793637e6195c01fa97fa0ad
47184 <a47184@alunos.isel.pt>
2023-03-09 01:37:06 +0000

    Data base beginning

--- src/main/kotlin/backend/sql/createSchema.sql
+++ src/main/kotlin/backend/sql/createSchema.sql
@@ -1,5 +1,31 @@
-create table EMPRESA (
+
+create schema if not exists dbo;
+
+create table if not exists dbo.EMPRESA (
+    email varchar(100) primary key,
+    password varchar(100),
+    tipo varchar(30),
+    descricao varchar(300),
+    beginhour timestamp,
+    endhour timestamp,
+    intervalo timestamp
  );
 
-create table CLIENTE (
+create table if not exists dbo.CLIENTE (
+    email varchar(100) primary key,
+    password varchar(100),
+    username varchar(30),
+    firstname varchar(20),
+    lastname varchar(20),
+    birthday timestamp
+);
+
+create table if not exists dbo.MARCACOES (
+    id serial primary key,
+    client varchar(100),
+    emp varchar(100),
+    date timestamp,
+    hour timestamp,
+    foreign key(client) references dbo.CLIENTE(email),
+    foreign key(emp) references dbo.EMPRESA(email)
 );
