9d286a4c63608c8d05e78a52b2fad890218302dd
47184 <a47184@alunos.isel.pt>
2023-06-01 22:49:46 +0100

    webpack.config update

--- frontend/my-app/src/index.tsx
+++ frontend/my-app/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ScheduleItApp from "./App";
 
@@ -10,7 +9,5 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <ScheduleItApp />
-  </React.StrictMode>
 );
