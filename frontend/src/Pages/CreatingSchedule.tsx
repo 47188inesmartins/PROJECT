f0206b8f54877f4315e9eac2ba4ff3f8ddb363ff
47184 <a47184@alunos.isel.pt>
2023-06-12 01:40:27 +0100

    add days and hour format change in backend

--- frontend/src/Pages/CreatingSchedule.tsx
+++ frontend/src/Pages/CreatingSchedule.tsx
@@ -3,17 +3,21 @@ import {useState} from "react";
 import "../Style/CreatingCompany.css"
 import "../Style/CreatingSchedule.css"
 import 'rc-picker/assets/index.css';
+import {Fetch} from "../Utils/useFetch";
+import {useParams} from "react-router-dom";
 
 
 export function CreatingSchedule() {
-    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const daysOfWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+
+    const [create, setCreate] = useState<boolean>(false)
 
     const [schedule, setSchedule] = useState(
         daysOfWeek.map(day => ({
-            dayOfWeek: day,
+            weekDays: day,
             selected: false,
-            startTime: '',
-            endTime: ''
+            beginHour: '',
+            endHour: ''
         }))
     );
 
@@ -29,8 +33,8 @@ export function CreatingSchedule() {
         const selectedDays = schedule.filter(day => day.selected);
 
         if (selectedDays.length > 0) {
-            const startTime = selectedDays[0].startTime;
-            const endTime = selectedDays[0].endTime;
+            const startTime = selectedDays[0].beginHour;
+            const endTime = selectedDays[0].endHour;
 
             setSchedule(prevSchedule =>
                 prevSchedule.map(day => {
@@ -51,72 +55,113 @@ export function CreatingSchedule() {
         });
     };
 
+    function FetchCreateDays(){
+        const params = useParams()
+        const id = params.id
+        const body =  schedule
+            .filter(day => day.beginHour !== '' && day.endHour !== '')
+            .map(day => ({
+                weekDays: day.weekDays,
+                beginHour: day.beginHour,
+                endHour: day.endHour
+            }))
+            const resp = Fetch(`company/${id}/day/all`,
+                'POST',
+                body).response
+            if(!resp) return(<p>...loading...</p>);
+            if(resp.status) {
+                setCreate(false)
+                window.location.href = `/`
+                return(<></>);
+            }
+            if(resp){
+                console.log(resp)
+                const companyId = resp.id
+                console.log("company = ", companyId)
+                window.location.href = `/company/${companyId}/schedule`
+                return(
+                    <></>
+                )
+        }
+    }
+
     function handleCreate(){
-       console.log(schedule)
-    };
+        setCreate(true)
+        console.log(
+            schedule
+                .filter(day => day.beginHour !== '' && day.endHour !== '')
+                .map(day => ({
+                    week_days: day.weekDays,
+                    begin_hour: day.beginHour,
+                    end_hour: day.endHour
+                }))
+        );
+    }
 
 
 
     return (
         <div>
-            <section className="vh-100 gradient-custom">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div className="card bg-dark text-white" style={{borderRadius: '1rem'}}>
-                                <div className="card-body p-5 text-center">
-                                    <div className="mb-md-5 mt-md-4 pb-5">
-                                        <h2 className="fw-bold mb-2 text-uppercase">
-                                            Personalize your schedule
-                                        </h2>
-                                        <br/><br/>
-                                        <div>
-                                        {schedule.map((day, index) => (
-                                            <div key={index}>
-                                                <label>
-                                                    <input
-                                                        type="checkbox"
-                                                        checked={day.selected}
-                                                        onChange={e => handleCheckboxChange(index, e.target.checked)}
-                                                    />
-                                                    {day.dayOfWeek}
-                                                </label>
-                                                <input
-                                                    type="time"
-                                                    value={day.startTime}
-                                                    onChange={e => handleTimeChange(index, 'startTime', e.target.value)}
-                                                />
-                                                <input
-                                                    type="time"
-                                                    value={day.endTime}
-                                                    onChange={e => handleTimeChange(index, 'endTime', e.target.value)}
-                                                />
+            {!create ?
+                <section className="vh-100 gradient-custom">
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                                <div className="card bg-dark text-white" style={{borderRadius: '1rem'}}>
+                                    <div className="card-body p-5 text-center">
+                                        <div className="mb-md-5 mt-md-4 pb-5">
+                                            <h2 className="fw-bold mb-2 text-uppercase">
+                                                Personalize your schedule
+                                            </h2>
+                                            <br/><br/>
+                                            <div>
+                                                {schedule.map((day, index) => (
+                                                    <div key={index}>
+                                                        <label>
+                                                            <input
+                                                                type="checkbox"
+                                                                checked={day.selected}
+                                                                onChange={e => handleCheckboxChange(index, e.target.checked)}
+                                                            />
+                                                            {day.weekDays}
+                                                        </label>
+                                                        <input
+                                                            type="time"
+                                                            value={day.beginHour}
+                                                            onChange={e => handleTimeChange(index, 'beginHour', e.target.value)}
+                                                        />
+                                                        <input
+                                                            type="time"
+                                                            value={day.endHour}
+                                                            onChange={e => handleTimeChange(index, 'endHour', e.target.value)}
+                                                        />
+                                                    </div>
+                                                ))}
+                                                <button onClick={handleSameTimeButtonClick}>Mesmo horário</button>
                                             </div>
-                                        ))}
-                                        <button onClick={handleSameTimeButtonClick}>Mesmo horário</button>
-                                    </div>
-                                        <br/><br/>
+                                            <br/><br/>
 
-                                        <button className="btn btn-outline-light btn-lg px-5" type="submit"
-                                                onClick={handleCreate}>
-                                            Create
-                                        </button>
-                                        <br/>
-                                        <br/>
-                                        <br/>
-                                        <button className="btn btn-outline-light btn-lg px-5" type="submit"
-                                                onClick={()=>{}}>Cancel
-                                        </button>
-
+                                            <button className="btn btn-outline-light btn-lg px-5" type="submit"
+                                                    onClick={handleCreate}>
+                                                Create
+                                            </button>
+                                            <br/>
+                                            <br/>
+                                            <br/>
+                                            <button className="btn btn-outline-light btn-lg px-5" type="submit"
+                                                    onClick={() => {
+                                                    }}>Cancel
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-
+                </section>
+                :
+                <FetchCreateDays/>
+            }
         </div>
     );
-
 }
--- src/main/kotlin/backend/jvm/services/dto/DayDto.kt
+++ src/main/kotlin/backend/jvm/services/dto/DayDto.kt
@@ -14,11 +14,11 @@ data class DayInputDto(
     val schedule: Int?,
     val service: Int?,
 ) {
-    fun mapToDayDb(dto: DayInputDto, schedule: Schedule?, service: ServiceDB?): Day {
-        val begin = Time.valueOf(dto.beginHour)?: throw Exception("invalid hour")
-        val end = Time.valueOf(dto.endHour) ?: throw Exception("invalid hour")
-        val intervalEnd = if(dto.intervalEnd != null) Time.valueOf(dto.intervalEnd) else null
-        val intervalBegin = if(dto.intervalBegin != null) Time.valueOf(dto.intervalBegin) else null
+    fun mapToDayDb(dto: DayInputDto,schedule: Schedule?, service: ServiceDB?): Day {
+        val begin = Time.valueOf(dto.beginHour.plus(":00"))?: throw Exception("invalid hour")
+        val end = Time.valueOf(dto.endHour.plus(":00")) ?: throw Exception("invalid hour")
+        val intervalEnd = if(dto.intervalEnd != null) Time.valueOf(dto.intervalEnd.plus(":00")) else null
+        val intervalBegin = if(dto.intervalBegin != null) Time.valueOf(dto.intervalBegin.plus(":00")) else null
 
         if(intervalEnd != null && intervalBegin != null ) {
             if (begin.after(intervalBegin)) throw Exception("invalid hours")
